52f9b86cd5e3a3159d16b6b44b3b895b7be99db8
MatinRah <matin.rahem@tekschool.us>
2023-02-22 18:15:50 -0500

    this is class practice

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -4,5 +4,10 @@ public class GitTest {
 	public void test() {
 		System.out.println("this is just a test");
 	}
+	
+	public void testTow() {
+		System.out.println("this is class practice");
+		
+	}
 
 }
