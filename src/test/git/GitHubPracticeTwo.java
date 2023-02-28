fc61552f201c261a4f4ed8918e5b5520c28064e2
MatinRah <matin.rahem@tekschool.us>
2023-02-27 21:04:04 -0500

    this is my code

---
+++ src/test/git/GitHubPracticeTwo.java
@@ -0,0 +1,15 @@
+package test.git;
+
+public class GitHubPracticeTwo {
+	
+	public int test (int a, int b)  {
+		return a*b;
+	}
+	
+	public int test (int x, int y, String message) {
+		System.out.println(message);
+		int z = x+y;
+		return z;
+	}
+
+}
